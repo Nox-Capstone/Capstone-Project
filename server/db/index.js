ed7bfab2d1955fad5fc13e87947cea0e83b2abf4
Ithndr <tm21999@gmail.com>
2023-02-23 10:49:15 -0500

    created db tables

--- server/db/index.js
+++ server/db/index.js
@@ -5,21 +5,71 @@ const {
   authenticate
 } = require('./User');
 
-const syncTables = async()=> {
+const dropTables = async () => {
   const SQL = `
+  DROP TABLE IF EXISTS reviews;
+  DROP TABLE IF EXISTS build_products;
+  DROP TABLE IF EXISTS build;
+  DROP TABLE IF EXISTS cart;
+  DROP TABLE IF EXISTS cart_products;
+  DROP TABLE IF EXISTS products;
   DROP TABLE IF EXISTS users;
+`
+  await client.query(SQL);
+}
+
+const syncTables = async () => {
+  const SQL = `
   CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL,
+    "isAdam" BOOLEAN default FALSE,
+    image VARCHAR(255)
+  );
+  CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price INTEGER,
+    quantity INTEGER,
+    image VARCHAR(255)
+  );
+  CREATE TABLE cart_products(
+    id SERIAL PRIMARY KEY,
+    "userId" INTEGER REFERENCES users(id),
+    "productsId" INTEGER REFERENCES products(id)
+  );
+  CREATE TABLE cart(
+    id SERIAL PRIMARY KEY,
+    quantity INTEGER,
+    subtotal INTEGER,
+    "cartProductsId" INTEGER REFERENCES cart_products(id)
+  );
+  CREATE TABLE build(
+    id SERIAL PRIMARY KEY,
+    "userId" INTEGER REFERENCES users(id),
+    "productsId" INTEGER REFERENCES products(id),
+    image VARCHAR(255)
+  );
+  CREATE TABLE build_products(
+    id SERIAL PRIMARY KEY,
+    "buildId" INTEGER REFERENCES build(id),
+    "userId" INTEGER REFERENCES users(id)
+  );
+  CREATE TABLE reviews(
+    id SERIAL PRIMARY KEY,
+    "userId" INTEGER REFERENCES users(id),
+    "productsId" INTEGER REFERENCES products(id)
   );
   `;
   await client.query(SQL);
 };
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
+  await dropTables();
   await syncTables();
-  const [moe, lucy]  = await Promise.all([
+  const [moe, lucy] = await Promise.all([
     createUser({
       username: 'moe',
       password: 'moe_password'
