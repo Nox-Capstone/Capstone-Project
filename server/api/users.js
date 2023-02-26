d8f1e29035757876ff6aeee587a15737dc6233fb
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-02-26 11:53:08 -0700

    added .env file, added .env to gitignore, api/users/login function started

--- server/api/users.js
+++ server/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 
 const { createUser } = require("../db");
+const { requireUserPass } = require("./utils");
 
 // POST /api/users/register
 router.post('/register', async (req, res, next) => {
@@ -22,13 +23,23 @@ router.post('/register', async (req, res, next) => {
                 message: "Password is less than 8 charactors"
             });
         } else {
-            const token = jwt.sign({ id: newUser.id, username: newUser.username})
+            const token = jwt.sign({ id: newUser.id, username: newUser.username}, process.env.JWT_SECRET);
             res.send({
                 newUser,
                 message: `The username ${username} has been registered successfully`, 
                 token
             });
         }
+
+    } catch (err) {
+        throw err;
+    }
+})
+
+// POST /api/users/login
+router.post('/login', requireUserPass, async (req, res, next) => {
+    try {
+        const { username, password } = req.body
 
     } catch (err) {
         throw err;
