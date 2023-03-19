eeac47d999dc9e607947a7b77dfa43023f6083e5
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-03-19 17:52:11 -0600

    issues with URL at fetch.js line 2. Unable to pull process.env.DATABASE_URL

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,4 @@
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/noxDB');
+const client = new pg.Client('postgres://localhost/noxDB' || process.env.DATABASE_URL);
 
 module.exports = client;
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,5 +1,5 @@
 //URL in process.env HERE
-const url = process.env.DATABASE_URL;
+const url = "/api"
 
 const fetchLogin = async (username, password) => {
     try {
